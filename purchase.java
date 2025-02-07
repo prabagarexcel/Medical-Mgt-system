ed9827b7efe1e8946910e034094945004f3f2ea4
prabagarexcel <prabagarexcel@gmail.com>
2025-02-07 13:48:52 +0530

    Update purchase.java

--- purchase.java
+++ purchase.java
@@ -1,4 +1,5 @@
 void main()
   {
-    System.out.println("Welcome to git hub"
+    System.out.println("Welcome to git hub, This is enableing the business in efficient manner");
+  
                        }
